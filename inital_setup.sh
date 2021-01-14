69b5e1db5f9a510aad575bdec9854c2bba11dd0a
Andrew Skea <andrewskea.as@gmail.com>
2021-01-14 11:25:49 +0000

    Update to setup script

--- inital_setup.sh
+++ inital_setup.sh
@@ -13,16 +13,30 @@ sudo npm install -y -g pm2
 nodejs -v
 pm2 status
 
+cd ~/
+mkdir sonos-spotify-jukebox && cd sonos-spotify-jukebox
+export HOME_DIR="$(pwd)"
+echo "Starting installation in $HOME_DIR"
+
 function get_jukebox() {
-  git clone https://gitlab.com/AndrewSkea/spotify-nfc-jukebox.git
-  cd spotify-nfc-jukebox
+  cd $HOME_DIR
+  git clone https://gitlab.com/AndrewSkea/spotify-nfc-jukebox.git .
   sudo -f cp spotify-client/bin/spotify-cli /usr/local/bin
   [ -x "$(command -v spotify-cli)" ]
   output="$(spotify-cli devices)"
   echo $output
   [[ $output =~ "raspotify" ]] && echo "spotifi-cli successfull installed"
-  export HOME_DIR=$(pwd)
-  echo $HOME_DIR
+}
+
+function create_python_env() {
+  cd $HOME_DIR
+  python3 -m venv jukeboxenv
+  source jukeboxenv/bin/activate
+  pip3 install -r requirements.txt
+}
+
+function activate_env(){ 
+  source $HOME_DIR/jukeboxenv/bin/activate
 }
 
 
@@ -30,7 +44,6 @@ function get_sonos_http_api() {
   cd $HOME_DIR
   git clone https://github.com/jishi/node-sonos-http-api.git
 }
-
 
 function run_sonos_http_api() {
   cd $HOME_DIR/node-sonos-http-api
@@ -40,18 +53,19 @@ function run_sonos_http_api() {
 
 function start_read_service() {
   cd $HOME_DIR/read-service
-  pm2 start Read.py --name "read-service"
+  activate_env
+  pm2 start Read.py --name "read-service" --interpreter $HOME_DIR/jukeboxenv/bin/python
 }
 
 function start_jukebox_admin(){
   cd $HOME_DIR/jukebox-web
-  python3 -m venv venv
-  source venv/bin/activate
-  pip3 install -r ../requirements.txt
-  pm2 start app.py --name "jukebox-service" --interpreter $HOME_DIR/jukebox-web/venv/bin/python
+  activate_env
+  pm2 start app.py --name "jukebox-service" --interpreter $HOME_DIR/jukeboxenv/bin/python
 }
 
 get_jukebox
+create_python_env
+activate_env
 get_sonos_http_api
 run_sonos_http_api
 start_read_service
